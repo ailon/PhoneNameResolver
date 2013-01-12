551bd98a30fe345935451ae47950b00eb7815442
ailon <ailon@ailon.org>
2013-01-12 16:12:47 +0200

    implemented resolving for Nokia models

--- PhoneNameResolver.cs
+++ PhoneNameResolver.cs
@@ -52,7 +52,6 @@ namespace Ailon.WP.Utils
                 var modelMetadata = nokiaLookupTable[lookupValue];
                 if (nokiaCanonicalModels.ContainsKey(modelMetadata.ModelId))
                 {
-                    result.CanonicalManufacturer = nokiaCanonicalModels[modelMetadata.ModelId].CanonicalManufacturer;
                     result.CanonicalModel = nokiaCanonicalModels[modelMetadata.ModelId].CanonicalModel;
                     result.Comments = modelMetadata.Description;
                     result.IsResolved = true;
@@ -64,16 +63,58 @@ namespace Ailon.WP.Utils
 
         private static Dictionary<string, CanonicalPhoneName> nokiaCanonicalModels = new Dictionary<string, CanonicalPhoneName>()
         {
-            { "Lumia920", new CanonicalPhoneName() { CanonicalManufacturer = "NOKIA", CanonicalModel = "Lumia 920" } } 
+            { "Lumia505", new CanonicalPhoneName() { CanonicalModel = "Lumia 505" } }, 
+            { "Lumia510", new CanonicalPhoneName() { CanonicalModel = "Lumia 510" } }, 
+            { "Lumia610", new CanonicalPhoneName() { CanonicalModel = "Lumia 610" } }, 
+            { "Lumia620", new CanonicalPhoneName() { CanonicalModel = "Lumia 620" } }, 
+            { "Lumia710", new CanonicalPhoneName() { CanonicalModel = "Lumia 710" } }, 
+            { "Lumia800", new CanonicalPhoneName() { CanonicalModel = "Lumia 800" } }, 
+            { "Lumia810", new CanonicalPhoneName() { CanonicalModel = "Lumia 810" } }, 
+            { "Lumia820", new CanonicalPhoneName() { CanonicalModel = "Lumia 820" } }, 
+            { "Lumia822", new CanonicalPhoneName() { CanonicalModel = "Lumia 822" } }, 
+            { "Lumia900", new CanonicalPhoneName() { CanonicalModel = "Lumia 900" } }, 
+            { "Lumia920", new CanonicalPhoneName() { CanonicalModel = "Lumia 920" } } 
         };
 
         private static Dictionary<string, ReportedNameMetadata> nokiaLookupTable = new Dictionary<string, ReportedNameMetadata>()
         {
+            // Lumia 505
+            { "LUMIA 505", new ReportedNameMetadata("Lumia505", null) },
+            // Lumia 510
+            { "LUMIA 510", new ReportedNameMetadata("Lumia510", null) },
+            { "NOKIA 510", new ReportedNameMetadata("Lumia510", null) },
+            // Lumia 610
+            { "LUMIA 610", new ReportedNameMetadata("Lumia610", null) },
+            { "LUMIA 610 NFC", new ReportedNameMetadata("Lumia610", "NFC") },
+            { "NOKIA 610", new ReportedNameMetadata("Lumia610", null) },
+            { "NOKIA 610C", new ReportedNameMetadata("Lumia610", null) },
+            // Lumia 620
+            { "LUMIA 620", new ReportedNameMetadata("Lumia620", null) },
+            { "RM-846", new ReportedNameMetadata("Lumia620", null) },
+            // Lumia 710
+            { "LUMIA 710", new ReportedNameMetadata("Lumia710", null) },
+            { "NOKIA 710", new ReportedNameMetadata("Lumia710", null) },
+            // Lumia 800
+            { "LUMIA 800", new ReportedNameMetadata("Lumia800", null) },
+            { "NOKIA 800", new ReportedNameMetadata("Lumia800", null) },
+            { "NOKIA 800C", new ReportedNameMetadata("Lumia800", "China") },
+            // Lumia 810
+            { "RM-878", new ReportedNameMetadata("Lumia810", "T-Mobile USA") },
+            // Lumia 820
+            { "RM-824", new ReportedNameMetadata("Lumia820", null) },
+            { "RM-825", new ReportedNameMetadata("Lumia820", null) },
+            { "RM-826", new ReportedNameMetadata("Lumia820", null) },
+            // Lumia 822
+            { "RM-845", new ReportedNameMetadata("Lumia822", "Verizon") },
+            // Lumia 900
+            { "LUMIA 900", new ReportedNameMetadata("Lumia900", null) },
+            { "NOKIA 900", new ReportedNameMetadata("Lumia900", null) },
             // Lumia 920
             { "RM-820", new ReportedNameMetadata("Lumia920", null) },
             { "RM-821", new ReportedNameMetadata("Lumia920", null) },
             { "RM-822", new ReportedNameMetadata("Lumia920", "China") },
             { "RM-867", new ReportedNameMetadata("Lumia920", "920T") },
+            { "NOKIA 920", new ReportedNameMetadata("Lumia920", null) },
         };
     }
 
